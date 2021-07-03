2fe9403b536d17e8c30bc3490bea56a48f0d3c01
alenj <alenjojo252001@gmail.com>
2021-07-03 15:35:31 +0530

    added github api

--- src/App.js
+++ src/App.js
@@ -4,48 +4,98 @@ import { Form, Card, Image, Icon } from 'semantic-ui-react';
 
 function App() {
   const [name, setName] = useState('');
+  const [userName, setUsername] = useState('');
+  const [followers, setFollowers] = useState('');
+  const [following, setFollowing] = useState('');
+  const [repos, setRepos] = useState('');
+  const [avatar, setAvatar] = useState('');
+  const [userInput, setUserInput] = useState('');
+  const [error, setError] = useState(null);
 
   useEffect(
     () => {
       fetch("https://api.github.com/users/example")
         .then(res => res.json())
         .then(data => {
-          console.log(data);
+          setData(data)
         })
-    }, []
-  )
+    }, []);
+
+  const setData = ({
+    name,
+    login,
+    followers,
+    following,
+    public_repos,
+    avatar_url
+  }) => {
+    setName(name);
+    setUsername(login);
+    setFollowers(followers);
+    setFollowing(following);
+    setRepos(public_repos);
+    setAvatar(avatar_url);
+  };
+
+  const handleSearch = (e) => {
+    setUserInput(e.target.value)
+  }
+  const handleSubmit = () => {
+    fetch(`https://api.github.com/users/${userInput}`)
+      .then(res => res.json())
+      .then(data => {
+        if (data.message) {
+          setError(data.message)
+        }
+        else {
+          setData(data);
+          setError(null);
+        }
+      })
+  }
 
   return (
     <div >
       <div className="navbar">Github Stats</div>
       <div className="search">
-        <Form >
+        <Form onSubmit={handleSubmit}>
           <Form.Group>
-            <Form.Input placeholder='Github user' name='github user' />
+            <Form.Input placeholder='Github user' name='github user' onChange={handleSearch} />
             <Form.Button content='Search' />
           </Form.Group>
         </Form>
       </div>
-      <diV className="card">
-        <Card>
-          <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
-          <Card.Content>
-            <Card.Header>Matthew</Card.Header>
-            <Card.Meta>
-              <span className='date'>Joined in 2015</span>
-            </Card.Meta>
-            <Card.Description>
-              Matthew is a musician living in Nashville.
-            </Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-            <a>
-              <Icon name='user' />
-              22 Friends
-            </a>
-          </Card.Content>
-        </Card>
-      </diV>
+      {error ? (<h1>{error}</h1>
+      ) : (
+        <diV className="card">
+          <Card>
+            <Image src={avatar} wrapped ui={false} />
+            <Card.Content>
+              <Card.Header>{name}</Card.Header>
+              <Card.Header>{userName}</Card.Header>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name='user' />
+                {followers} Followers
+              </a>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name='user' />
+                {repos} Repos
+              </a>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name='user' />
+                {following} Following
+              </a>
+            </Card.Content>
+          </Card>
+        </diV>
+      )}
+
     </div>
   );
 }
